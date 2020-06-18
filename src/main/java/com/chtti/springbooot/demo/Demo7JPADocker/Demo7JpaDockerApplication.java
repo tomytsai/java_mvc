c5da32b49d9e596c301d4ea09cfd01b42bd73662
tomytsai <tomytsai@tomytsai>
2020-06-18 13:52:17 +0800

    Initial project setup from start.io

---
+++ src/main/java/com/chtti/springbooot/demo/Demo7JPADocker/Demo7JpaDockerApplication.java
@@ -0,0 +1,13 @@
+package com.chtti.springbooot.demo.Demo7JPADocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo7JpaDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo7JpaDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chtti/springbooot/demo/Demo7JPADocker/Demo7JpaDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chtti.springbooot.demo.Demo7JPADocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo7JpaDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
