f462d643bb43b12829153460407876e46c6a2502
tomytsai <tomytsai@tomytsai>
2020-06-19 16:05:13 +0800

    add data (with happy path)

--- src/main/java/com/chtti/springbooot/demo/Demo7JPADocker/controller/CustomerController.java
+++ src/main/java/com/chtti/springbooot/demo/Demo7JPADocker/controller/CustomerController.java
@@ -1,9 +1,15 @@
 package com.chtti.springbooot.demo.Demo7JPADocker.controller;
 
+import com.chtti.springbooot.demo.Demo7JPADocker.beans.Customer;
+import com.chtti.springbooot.demo.Demo7JPADocker.forms.CustomerForm;
 import com.chtti.springbooot.demo.Demo7JPADocker.service.CustomerService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,6 +17,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class CustomerController {
     @Autowired
     CustomerService service;
+
+    @ModelAttribute
+    CustomerForm setupForm(){
+        return new CustomerForm();
+    }
+
+    @RequestMapping(value="/customers/create", method = RequestMethod.POST)
+    String create(@Validated CustomerForm form, BindingResult result,Model model){
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(form,customer);
+        service.saveCustomer(customer);
+        return "redirect:/customers";
+    }
+
     @RequestMapping(value= "customers",
             method= RequestMethod.GET)
     String listAllP(Model model){
---
+++ src/main/java/com/chtti/springbooot/demo/Demo7JPADocker/forms/CustomerForm.java
@@ -0,0 +1,9 @@
+package com.chtti.springbooot.demo.Demo7JPADocker.forms;
+
+import lombok.Data;
+
+@Data
+public class CustomerForm {
+    private String firstName;
+    private String lastName;
+}
